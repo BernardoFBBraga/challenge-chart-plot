8994b97f96d6c81c46b45188d04be561c35740ea
Bernardo F B Braga <bernardofbbraga@gmail.com>
2018-03-26 15:20:20 -0300

    Customized the theme to match the design

--- src/Components/Editor/index.js
+++ src/Components/Editor/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MonacoEditor from 'react-monaco-editor';
+import './Editor.css'
 
 const requireConfig = {
 	url: '/require.min.js',
@@ -11,6 +12,23 @@ const requireConfig = {
 export default class Editor extends React.Component {
 	editorDidMount = (editor, monaco) => {
 		this.props.setEditor(editor)
+		monaco.editor.defineTheme('IntelieTheme', {
+			base: 'vs-dark',
+			inherit: true,
+			rules: [
+				{ token: 'number', foreground: 'b37edf' },
+				{ token: 'number.hex', foreground: 'b37edf' },
+				{ token: 'string.key.json', foreground: 'ffffff' },
+				{ token: 'string.value.json', foreground: '09daea' },
+			],
+			colors: {
+					'editor.background': '#2e3440',
+					'editor.lineHighlightBackground': '#0000FF20',
+					'editorLineNumber.foreground': '#313743',
+					'editorLineNumber.background': '#60656e',
+			}
+	});
+	monaco.editor.setTheme('IntelieTheme');
 	}
 	render() {
 		return (
@@ -21,7 +39,6 @@ export default class Editor extends React.Component {
 					width={this.props.width}
 					height={this.props.height}
 					language="json"
-					theme="vs-dark"
 					requireConfig={requireConfig}
 					editorDidMount={this.editorDidMount}
 					automaticLayout={false}
